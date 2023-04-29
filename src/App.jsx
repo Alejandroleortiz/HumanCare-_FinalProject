b6346d618c8b9494aba5097cc47396f00b7abd97
Ronald <ronald.micros@gmail.com>
2023-04-29 10:18:33 -0400

    Proyecto Final - Creacion de Account & AccountPages - 29/04/23

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>} />
+        
         <Route path='/dashboard' element={<Dashboard/>} />
         <Route path='/medical-records' element={<MedicalRecords/>} />
         <Route path='/patients' element={<Patients/>}/>
