5f2805cf2c9cd674654b4ed0ab38ffe9a47b9da0
Alejandro Leon <alejandroleortiz57@gmail.com>
2023-04-29 14:01:01 -0400

    agregado de rutas

--- api/models.py
+++ api/models.py
@@ -4,8 +4,15 @@ db=SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), nullable = False, unique=True)
-    password = db.Column(db.String(120), nullable = False, unique=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    profile_information = db.Column(db.String(200), nullable=False)
+    profile_picture = db.Column(db.String(150), nullable=False)
+    roles_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable =False)
+    medical_record = db.relationship('Medical_record',  backref="user", uselist = False)
+    medical_appoinments = db.relationship('Medical_appoinment', foreign_keys='[Medical_appoinment.doctor_id]', backref='doctor')
+    patient_appoinments = db.relationship('Medical_appoinment', foreign_keys='[Medical_appoinment.user_id]', backref='patient')
+
     
     
     def serialize(self):
@@ -13,8 +20,130 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             "password": self.password,
+            "profile_information": self.profile_information,
         }
     
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    users = db.relationship('User',  backref="role")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+        
+class Medical_file(db.Model):
+    __tablename__ = 'medical_file'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    file = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.DateTime(), default=db.func.now(), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "file": self.file,
+            "users_to_id": self.users_to_id,
+            "date": self.date,
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Medical_record(db.Model):
+    __tablename__ = 'medical_record'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    full_name = db.Column(db.String(150), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    identity_card = db.Column(db.Integer, nullable=False)
+    adress = db.Column(db.String(200), nullable=False)
+    phone_number =db.Column(db.Integer, nullable=False)
+    reason_for_consultation = db.Column(db.String(200), nullable=False)
+    current_illness = db.Column(db.String(200), nullable=False)
+    criminal_record = db.Column(db.String(200), nullable=False)
+    family_history = db.Column(db.String(200), nullable=False)
+    surgical_history = db.Column(db.String(200), nullable=False)
+    physical_examination = db.Column(db.String(500), nullable=False)
+    diagnosis = db.Column(db.String(200), nullable=False)
+    treatment = db.Column(db.String(200), nullable=False)
+    observations = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime(), default=db.func.now(), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "full_name": self.full_name,
+            "age": self.age,
+            "identity_card": self.identity_card,
+            "adress": self.adress,
+            "phone_number": self.phone_number,
+            "reason_for_consultation": self.reason_for_consultation,
+            "current_illness": self.current_illness,
+            "criminal_record": self.criminal_record,
+            "family_history": self.family_history,
+            "surgical_history": self.surgical_history,
+            "physical_examination": self.physical_examination,
+            "diagnosis": self.diagnosis,
+            "treatment": self.treatment,
+            "observations": self.observations,
+            "date": self.date,
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Medical_appoinment(db.Model):
+    __tablename__ = 'medical_appoinment'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    file = db.Column(db.String(150), nullable=False)
+    comment = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime(), default=db.func.now(), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "doctor_id": self.doctor_id,
+            "file": self.file,
+            "comment": self.comment,
+            "date": self.date,
+        }
+
     def save(self):
         db.session.add(self)
         db.session.commit()
--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,13 +1,73 @@
 from datetime import datetime
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
+from models import User, Role
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bpAuth = Blueprint('bpAuth', __name__)
 
 @bpAuth.route('/signup', methods=['POST']) #register
 def signup():
-    return jsonify({"status":""})
+    
+    # Datos de la tabla "users"
+    email = request.json.get('email')
+    password = request.json.get('password')
+    role = request.json.get('roles_id') # Datos de la tabla roles
+    
+    if not email:
+        return jsonify({ "msg": "email is required"}), 422
 
-@bpAuth.route('/signup', methods=['POST']) #login
+    if not password:
+        return jsonify({ "msg": "Password is required"}), 422
+    
+    if not role:
+        return jsonify({ "msg": "Role is required"}), 422
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        return jsonify({ "msg": "User already exists"}), 400
+
+    
+    user = User()
+    user.email = email
+    user.password = generate_password_hash(password)
+
+    
+    # Token sin vencimiento
+    access_token = create_access_token(identity=user.id)
+
+    data = {
+        "access_token": access_token,
+        "user": user.serialize()
+    }
+
+    return jsonify(data), 200
+
+@bpAuth.route('/signin', methods=['POST']) #login
 def signin():
-    pass
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not email:
+        return jsonify({ "msg": "email is required"}), 422
+
+    if not password:
+        return jsonify({ "msg": "Password is required"}), 422
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return jsonify({ "msg": "email/Password are incorrects"}), 401
+
+    if not check_password_hash(user.password, password):
+        return jsonify({ "msg": "email/Password are incorrects"}), 401
+    
+    access_token = create_access_token(identity=user.id)
+
+    data = {
+        "access_token": access_token,
+        "user": user.serialize()
+    }
+
+    return jsonify(data), 200
